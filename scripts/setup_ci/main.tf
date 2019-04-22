d41dfc6ecc4b9b8ffdf574a59807842871e1c041
Irbe Krumina <irbekrm@gmail.com>
2019-04-22 18:23:17 +0100

    Creates a VPC and an EC2 instance

--- scripts/setup_ci/main.tf
+++ scripts/setup_ci/main.tf
@@ -15,10 +15,60 @@ resource "tls_private_key" "ssh-key" {
 }
 
 resource "aws_key_pair" "ssh-key-aws" {
-    key_name = "${var.project}-ci-keys"
+    key_name = "${var.project}-ci"
     public_key = "${tls_private_key.ssh-key.public_key_openssh}"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+      Name = "${var.project}-ci"
+  }
+}
+
+resource "aws_subnet" "ci" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.0.0/24"
+
+    tags = {
+        Name = "${var.project}-ci"
+    }
+}
+
+resource "aws_security_group" "concourse-security-group" {
+    name = "${var.project}-ci"
+    description = "Allow traffic to the EC2 instance hosting Concourse"
+    vpc_id = "${aws_vpc.main.id}"
+
+    ingress {
+        from_port = 8080
+        to_port = 8080
+        protocol = "tcp"
+        cidr_blocks = "${var.concourse-whitelisted-cidr-ranges}"
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = "tcp"
+        cidr_blocks = "${var.ssh-whitelisted-cidr-ranges}"
+    }
+}
+
+resource "aws_instance" "ec2-instance" {
+    ami = "${var.ec2-ami}"
+    instance_type = "${var.ec2-instance-type}"
+    key_name = "${aws_key_pair.ssh-key-aws.key_name}"
+    subnet_id = "${aws_subnet.ci.id}"
+    vpc_security_group_ids = ["${aws_security_group.concourse-security-group.id}"]
+    tags = {
+        Name = "${var.project}-ci"
+    }
 }
 
 output "ssh-private-key" {
     value = "${tls_private_key.ssh-key.private_key_pem}"
+}
+
+output "ec2-ip" {
+    value = "${aws_instance.ec2-instance.public_ip}"
 }
--- scripts/setup_ci/variables.tf
+++ scripts/setup_ci/variables.tf
@@ -5,3 +5,21 @@ variable "project" {
 variable "region" {
     default = "eu-west-1"
 }
+
+variable "ssh-whitelisted-cidr-ranges" {
+    type = "list"
+    default = ["86.182.149.104/32"]
+}
+
+variable "concourse-whitelisted-cidr-ranges" {
+    type = "list"
+    default = ["86.182.149.104/32"]
+}
+
+variable "ec2-ami" {
+    default = "ami-07683a44e80cd32c5"
+}
+
+variable "ec2-instance-type" {
+    default = "t2.micro"
+}
