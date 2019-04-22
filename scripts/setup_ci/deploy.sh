d2718717dcffb00d88fbfb01ebac5a97effc0b8b
Irbe Krumina <irbekrm@gmail.com>
2019-04-22 18:28:08 +0100

    Better wording

--- scripts/setup_ci/deploy.sh
+++ scripts/setup_ci/deploy.sh
@@ -3,7 +3,7 @@
 
 export AWS_DEFAULT_REGION=eu-west-1
 
-# Will deploy an AWS S3 storage bucket. This is where the Terraform will store its state (references to what AWS resources it has created)
+# Deploys an AWS S3 storage bucket. This is where the Terraform will store its state (references to what AWS resources it has created)
 aws s3 mb s3://quikquix-ci-tfstate
 
 terraform init -auto-approve
