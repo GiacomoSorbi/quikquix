24d91ce90f52edf29406d7bed3b5fe3eb88ec7c6
Irbe Krumina <irbekrm@gmail.com>
2019-04-24 21:50:04 +0100

    removes the unused source ip variable

--- scripts/setup_ci/deploy.sh
+++ scripts/setup_ci/deploy.sh
@@ -5,8 +5,6 @@ export AWS_DEFAULT_REGION=eu-west-1
 # Deploys an AWS S3 storage bucket. This is where the Terraform will store its state (references to what AWS resources it has created)
 aws s3 mb s3://quikquix-tf
 
-source_ip=$(curl ifconfig.me)
-
 terraform init -input=false
 
 terraform apply -input=false -auto-approve
